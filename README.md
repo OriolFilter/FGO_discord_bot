7fda9a514471bad6af2220b063895eab8ddf65d4
OriolFilter <filter.oriol@gmail.com>
2020-01-26 02:38:10 +0100

    v1.01 more info in README.md

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # FGO_bot
+
+##Commands aviable
+
+###Events
+
+    f.events    Returns a link to a event list (USA)
+    f.aevents   Returns a list of 'active' events and a short note with a link to check information from each event    
+    f.aevents   Returns a list of 'future' events and a short note with a link to check information from each event    
+    
+
+####Servants related
+
+#####Class
+
+    f.class         Returns a list of classes aviable and their respective links
+    f.saber         Returns his respective class link
+    f.archer        Returns his respective class link
+    f.lancer        Returns his respective class link
+    f.rider         Returns his respective class link
+    f.caster        Returns his respective class link
+    f.assassin      Returns his respective class link
+    f.berserker     Returns his respective class link
+    f.shielder      Returns his respective class link
+    f.ruler         Returns his respective class link
+    f.avenger       Returns his respective class link
+    f.mooncancer    Returns his respective class link
+    f.foreigner     Returns his respective class link
+    f.beast         Returns his respective class link
+    
+###Misc
+   
+    f.time      Returns current time of the server (PST hour)
+    f.invite    Returns a link to invite this bot to your own discord-server
+    f.help      Returns help message
+
+##Versions
+
+###1.0
+
+Added commands:
+
+    - f.events
+    - f.aevents
+    - f.fevents
+    - f.time
+    - f.testing (debugg)
+    - f.invite
+    - f.help (just added the command, but it returns 'in progress'
+
+###1.01
+
+Removed commands:
+
+    - Removed 'testing' command
+    
+Added commands:
+
+    - classes
+    - saber
+    - archer
+    - lancer
+    - caster
+    - rider
+    - assassin
+    - berserker
+    - shielder
+    - ruler
+    - avenger
+    - mooncancer
+    - alterego
+    - foreigner
+    - beast
+    
+    
+##Disclaimer
+
+Im not owner of any of the content used (links, images, etc)
+In case the owner want me to remove his content from the bot usage leave a comment on discord or contact me via Twitter
+
+##How to use this github files
+
+At the file "credentials.py", change the 'Token' value, to your own discord-bot api key then run the code with python.
+
+All the code was tested with python 3.7, can't promise it will work with older versions. 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import discord
+import os
 from discord.ext import commands
 from discord.utils import get
 from credentials import TOKEN
@@ -13,6 +14,10 @@ description = '''Master im waiting for you to summon me!'''
 bot = commands.Bot(command_prefix="f.", description=description)
 bot.remove_command('help')
 
+#Variables
+where_i_im=os.path.dirname(os.path.abspath(__file__)) #where is the file/folder of this python document...
+help_p1_file_location=where_i_im+"/help_pt1.txt"
+
 @bot.event
 async def on_ready():
     print('------')
@@ -23,16 +28,12 @@ async def on_ready():
     game = discord.Game("I will help you master, type \"f.help\" if you still need learn to summon me!")
     await bot.change_presence(status=discord.Status.online, activity=game)
 
-@bot.command()
-async def test(ctx):
-    await ctx.send("testing...")
-
 ## Main_Commands
 
+#Events
 @bot.command()
 async def events(ctx):
     await ctx.send("Hi master, you can check the active and future events at this webpage:\nhttps://fategrandorder.fandom.com/wiki/Event_List_(US)")
-    #await ctx.send("Hi master, you can check the active and future events at this webpage:\nLINK")
 
 @bot.command()
 async def aevents(ctx): #active events
@@ -65,13 +66,76 @@ async def fevents(ctx): #future events
     await ctx.send(embed=tale_of_setsubun)
 
 
+#Class
 @bot.command()
-async def invite(ctx):
-    await ctx.send("Master summon me in your own server!\nhttps://discordapp.com/oauth2/authorize?client_id=668594429573070849&scope=bot&permissions=478272")
+async def classes(ctx):
+    await ctx.send("Master i see you are in a search of knowledge, let me help you with this books!\n\nSaber:\t\t         <https://fategrandorder.fandom.com/wiki/Saber>\nLancer:\t\t       <https://fategrandorder.fandom.com/wiki/Lancer>\nArcher:\t\t       <https://fategrandorder.fandom.com/wiki/Archer>\nRider:\t\t          <https://fategrandorder.fandom.com/wiki/Rider>\nCaster:\t\t        <https://fategrandorder.fandom.com/wiki/Caster>\nAssassin:\t\t    <https://fategrandorder.fandom.com/wiki/Assassin>\nBerserker:\t\t   <https://fategrandorder.fandom.com/wiki/Berserker>\nShielder:\t\t      <https://fategrandorder.fandom.com/wiki/Shielder>\nRuler:\t\t\t       <https://fategrandorder.fandom.com/wiki/Ruler>\nAvenger:\t\t\t <https://fategrandorder.fandom.com/wiki/Avenger>\nMoon Cancer:\t<https://fategrandorder.fandom.com/wiki/Moon_Cancer>\nForeigner:\t\t    <https://fategrandorder.fandom.com/wiki/Foreigner>\nBeast:\t\t\t       <https://fategrandorder.fandom.com/wiki/Beast>")
 
 @bot.command()
+async def saber(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Saber")
+
+@bot.command()
+async def lancer(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Lancer")
+
+@bot.command()
+async def archer(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Archer")
+
+@bot.command()
+async def caster(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Caster")
+
+@bot.command()
+async def assassin(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Assassin")
+
+@bot.command()
+async def rider(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Rider")
+
+@bot.command()
+async def berserker(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Berserker")
+
+@bot.command()
+async def shielder(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Shielder")
+
+@bot.command()
+async def ruler(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Ruler")
+
+@bot.command()
+async def avenger(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Avenger")
+
+@bot.command()
+async def mooncancer(ctx): #moon cancer
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Moon_Cancer")
+
+@bot.command()
+async def alterego(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Alter_Ego")
+
+@bot.command()
+async def foreigner(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Foreigner")
+
+@bot.command()
+async def beast(ctx):
+    await ctx.send("Here is what you asked for master!\nhttps://fategrandorder.fandom.com/wiki/Beast")
+
+#Misc
+@bot.command()
 async def help(ctx):
-    await ctx.send ("In progress")
+    help_p1 = open((help_p1_file_location), "r")
+    help_msp1 = help_p1.read()
+    help_p1.close()
+    await ctx.author.send(help_msp1)
+
+
 
 @bot.command()
 async def time(ctx):
@@ -80,6 +144,11 @@ async def time(ctx):
     time = time.astimezone(timezone('US/Pacific'))
     pstTime=time.strftime(time_format)
     await ctx.send("PST time: "+pstTime)
+
+
+@bot.command()
+async def invite(ctx):
+    await ctx.send("Master use this to summon me!\nhttps://discordapp.com/oauth2/authorize?client_id=668594429573070849&scope=bot&permissions=478272")
 
 ## extra
 bot.run(TOKEN)
